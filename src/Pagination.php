a51bfa0855557d29852e0500ff613df998781728
Makan <makannikov@gmail.com>
2023-01-12 17:19:02 +0900

    feat: Refactored pagination class

--- src/Pagination.php
+++ src/Pagination.php
@@ -4,74 +4,76 @@ namespace Makan\QueryBuilder;
 class Pagination
 {
     /**
-     * @var int Текущая страница
+     * @var int $currentPage Текущая страница
+     * @var int $results Количество результатов на странице
      */
-    private int $currentPage;
-
-    /**
-     * @var int Количество результатов на странице
-     */
-    public int $results = 20;
-    public int $items; // Количество публиркаций
-    private $request_uri; // Параметры адресной строки
     private string $url; // Ссылка для страниц
     public int $start;
-    private int $qty_pages;
+    private int $totalPages;
     public array $tmpl;
 
     const PAGINATION_KEY_ACTION = 'page';
     const PAGINATION_LINKS = 2;
     const DEFAULT_PAGE_RESULTS = 15;
+    private string $link;
 
 
     /**
      * @param int $items Всего строк в базе
-     * @param int $page Текущая страница
-     * @param int|null $results Кол-во строк на странице
+     * @param int $currentPage Текущая страница
+     * @param int $results Кол-во строк на странице
      */
-    public function __construct(int $items, int $page = 1, int $results = null)
+    public function __construct(
+        public int $items,
+        private int $currentPage = 1,
+        public int $results = self::DEFAULT_PAGE_RESULTS)
     {
 
-        $this->items = $items;
-        $this->currentPage = $page;
-
-        // Настраиваем количество результатов на странице
-        $this->per_page($results);
-
-        $this->qty_pages();
+        $this->totalPages();
         // Формируем url и получаем текущую страницу
-        $this->url();
-        // Вычисление стартовой позиции
-        $this->get_start_position();
+        $this->setUrl();
 
+        //TODO Подумать как грамотно обработать этот кейс
+        if ($this->currentPage <= 0) {
+            $this->currentPage = 1;
+        }
+        //return ($this->currentPage > $this->totalPages and $this->totalPages != 0);
+
+        // Вычисление стартовой позиции
+        $this->getStartPosition();
         $this->init_links();
     }
 
+    public function getCurrentPage(){
+        return $this->currentPage;
+    }
 
-    public function url()
+    public function getTotalPages(){
+        return $this->totalPages;
+    }
+
+    public function getUrl($num){
+
+        $page = '';
+
+        if($num != 1)
+            $page = (str_contains($this->url, '?') ?  '&' : '?') . self::PAGINATION_KEY_ACTION . '=' . $num;
+
+        return $this->url . $page;
+    }
+
+
+    /**
+     * @return void форммируем url с ?page=? и без него
+     */
+    public function setUrl(): void
     {
         // Получаем текущиий URL
         // Если появились кавычки переводим их в сущьности и ссылка становится битая
-        if ($pos = strpos($_SERVER['REQUEST_URI'], '?')) {
-            $this->request_uri = htmlspecialchars(substr($_SERVER['REQUEST_URI'], 0, $pos));
-        } else
-            $this->request_uri = htmlspecialchars($_SERVER['REQUEST_URI']);
-
-            //$this->request_uri = urldecode(htmlspecialchars($_SERVER['REQUEST_URI']));
-        $pos = strpos($this->request_uri, '/' . urlencode(self::PAGINATION_KEY_ACTION));
-        if ($pos > 0) {
-            $this->url = mb_substr($this->request_uri, 0, $pos, 'UTF-8');
-        } else {
-            $this->url = $this->request_uri;
-        }
-
-        if (!$this->currentPage or $this->currentPage < 0) {
-            $this->currentPage = 1;
-        }
-
-        return ($this->currentPage > $this->qty_pages and $this->qty_pages != 0);
-
+        /** Remove page=n from uri string and clean characters */
+        $this->url =  htmlspecialchars(preg_replace("/[?,&]?page=\d+/", '', $_SERVER['REQUEST_URI']));
     }
+
 
     public function limit()
     {
@@ -83,75 +85,63 @@ class Pagination
         return $this->start;
     }
 
-    public function getStarFinish()
-    {
-        $arr['start'] = $this->start;
-        $arr['limit'] = $this->results;
-        return $arr;
-    }
 
-
-    private function get_start_position()
+    private function getStartPosition()
     {
-// Вычисляем стартовую позицию
+        // Вычисляем стартовую позицию
         $this->start = $this->currentPage * $this->results - $this->results;
-// 0 * 3 - 3 = 0
-// 1 * 3 - 3 = 0
-// 2 * 3 - 3 = 3
-// 3 * 3 - 3 = 6
-// 4 * 3 - 3 = 9
-// 5 * 3 - 3 = 12
-// 6 * 3 - 3 = 15
-// 7 * 3 - 3 = 18
-// 8 * 3 - 3 = 21
-// 9 * 3 - 3 = 24
-// 10 * 3 - 3 = 27
+        // 0 * 3 - 3 = 0
+        // 1 * 3 - 3 = 0
+        // 2 * 3 - 3 = 3
+        // 3 * 3 - 3 = 6
+        // 4 * 3 - 3 = 9
+        // 5 * 3 - 3 = 12
+        // 6 * 3 - 3 = 15
+        // 7 * 3 - 3 = 18
+        // 8 * 3 - 3 = 21
+        // 9 * 3 - 3 = 24
+        // 10 * 3 - 3 = 27
     }
 
-    private function qty_pages()
+    private function totalPages()
     {
         // Округляем в большую сторону
-        $this->qty_pages = ceil($this->items / $this->results);
-        //echo '<pre>$this->items ', print_r($this->items, true), '</pre>';
-        //echo '<pre>$this->results ', print_r($this->results, true), '</pre>';
-        //echo '<pre>$this->qty_pages ', print_r($this->qty_pages, true), '</pre>';
-        //exit(PHP_EOL . __FILE__ . '::' . __LINE__ . PHP_EOL);
+        $this->totalPages = ceil($this->items / $this->results);
+    }
+
+    public function right(int $num = self::PAGINATION_LINKS) :array{
+
+        for ($i = 1; $i <= $num; $i++) {
+            if (($this->currentPage + $i) <= $this->totalPages) {
+                $right[] = $this->currentPage + $i;
+            }
+        }
+        return $right ?? [];
+    }
+
+    public function left(int $num = self::PAGINATION_LINKS) :array{
+
+        for ($i = $num; $i >= 1; $i--) {
+            if ($this->currentPage - $i >= 1) {
+                $left[] = $this->currentPage - $i;
+            }
+        }
+        return $left ?? [];
+    }
+    public function prev() :int {
+        return ($this->currentPage <= 1) ? 0 : $this->currentPage - 1;
+    }
+
+    public function next(): int
+    {
+        return ($this->currentPage < $this->totalPages) ? $this->currentPage + 1 : 0;
     }
 
 
     public function init_links()
     {
-        $this->tmpl = array();
-
-        $this->tmpl['current'] = $this->currentPage;
-        $this->tmpl ['results'] = $this->results;
-        $this->tmpl['url'] = $this->url;
-        $this->tmpl['qty_pages'] = $this->qty_pages;
-        $this->tmpl['after'] = '';
+        //TODO Зачем это нужно?
         if (isset($_SERVER['REDIRECT_QUERY_STRING'])) $this->tmpl['after'] = '?' . $_SERVER['REDIRECT_QUERY_STRING'];
 
-
-        // Проверяем нужны ли стрелки назад
-        if ($this->currentPage != 1) $this->tmpl['prev'] = $this->currentPage - 1;
-        // Проверяем нужны ли стрелки вперед
-        if ($this->currentPage < $this->qty_pages) $this->tmpl['next'] = $this->currentPage + 1;
-
-
-        if (self::PAGINATION_LINKS) {
-            // Генерируем номера следующих страниц
-            for ($i = 1; $i <= self::PAGINATION_LINKS; $i++) {
-                if (($this->currentPage + $i) <= $this->qty_pages) $this->tmpl['right'][] = $this->currentPage + $i;
-            }
-
-            // Генерируем номера предыдущих страниц
-            for ($i = self::PAGINATION_LINKS; $i >= 1; $i--) {
-                if ($this->currentPage - $i >= 1) $this->tmpl['left'][] = $this->currentPage - $i;
-            }
-        }
-    }
-
-    private function per_page(int $perPageResults)
-    {
-        $this->results = $perPageResults ?? self::DEFAULT_PAGE_RESULTS;
     }
 }
